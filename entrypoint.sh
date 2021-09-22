f7710efbb88b353728960f2c475da88d9b922d71
Nizar Ajroud <nizar.rwk@gmail.com>
2021-09-22 22:24:12 +0200

    add docker chnage s

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+set -euo pipefail
+
+WORKDIR=${WORKDIR:-/github/workspace}
+cd ${WORKDIR}
+
+# retrieve and install jsonresume theme
+wget "$1" -O /theme.tgz
+npm install /theme.tgz
+package_name=$(npm pack /theme.tgz --dry-run 2>&1 | grep "^npm notice name:" | sed 's/npm notice name:\([ ]*\)\(.*\)/\2/')
+
+# export to HTML
+resume export -t ./node_modules/$package_name resume.html
+
+# generate PDF
+resume export -t ./node_modules/$package_name resume.pdf
